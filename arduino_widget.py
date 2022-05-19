e40b16ed41d7ffdb751d247f9b8b58d436f2d182
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2022-05-19 14:07:55 -0400

    basic arduino widget example

---
+++ arduino_widget.py
@@ -0,0 +1,43 @@
+import sys
+
+import serial
+from qtpy import QtWidgets as QtW
+from qtpy.QtCore import Qt
+
+
+class FakeLaser:
+    def __init__(self, port, baudrate=115200):
+        self._arduino = serial.Serial(port, baudrate=baudrate)
+
+    def write_serial(self, msg: str):
+        self._arduino.write(msg.encode())
+
+
+class LaserWidget(QtW.QWidget):
+    def __init__(self, laser: FakeLaser, parent=None):
+        super(LaserWidget, self).__init__(parent)
+        self.laser = laser
+        layout = QtW.QVBoxLayout()
+        self.l1 = QtW.QLabel("LED Brightness (0-255)")
+        self.l1.setAlignment(Qt.AlignCenter)
+        layout.addWidget(self.l1)
+
+        self.sb = QtW.QSpinBox()
+        self.sb.setMinimum(0)
+        self.sb.setMaximum(255)
+        self.sb.setValue(125)
+        self.sb.setKeyboardTracking(False)
+
+        layout.addWidget(self.sb)
+        self.sb.valueChanged.connect(lambda val: self.laser.write_serial(str(val)))
+        self.sb.valueChanged.connect(lambda val: print(val))
+        self.setLayout(layout)
+        self.setWindowTitle("Demo Custom Widget")
+
+
+if __name__ == "__main__":
+    app = QtW.QApplication(sys.argv)
+    led = FakeLaser("/dev/ttyACM0")
+    slider = LaserWidget(led)
+    slider.show()
+    sys.exit(app.exec_())
